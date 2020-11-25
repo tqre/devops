108c81b1551d1c6ad559db3fb755b03bee73cecf
tqre <tkuure@gmail.com>
2020-11-25 15:07:45 +0200

    Encrypted secrets - helper script, playbook, and variables file

---
+++ encrypt_vars.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Helper script to generate encrypted variables to GitLab installation playbook
+
+# List of the files to be created into secrets/ -directory
+FILES=(
+	gitlab_secret
+	gitlab_shell_secret
+	secret_key_base
+	db_key_base
+	otp_key_base
+	openid_connect_signing_key
+)
+
+# Check that none the files exist, as we don't want to overwrite any
+for file in ${FILES[@]};do
+  if [[ ! -f secrets/$file ]]; then continue
+    else echo "Some files exist already, aborting..."; exit
+  fi
+done
+
+# Generate random strings into each file
+for file in ${FILES[@]}; do
+  hexdump -v -n 64 -e '1/1 "%02x"' /dev/urandom > secrets/$file
+done
+
+# Encrypt strings with ansible-vault
+for file in ${FILES[@]}; do
+  ansible-vault encrypt_string $(cat secrets/$file) --name $file >> provision/encrypted_variables.yml
+done
+
+echo "Playbook -ready encrypted variables are in provision/encrypted_variables.yml -file"
