e2dde045f07ead29896579d61ed1d68932376fbf
tqre <tkuure@gmail.com>
2020-11-29 21:56:24 +0200

    Runner token and configuration

--- encrypt_tokens.sh
+++ encrypt_tokens.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-# Encrypt GitLab API-tokens and user data to a variable file
+# Encrypt GitLab API-tokens and a regular user secrets to a variable file
 
-if [[ ! -f secrets/api_token ]]; then
-  echo "No api_token found from secrets/ -directory! Aborting..."; exit
-fi
-
-ansible-vault encrypt_string $(<secrets/api_token) --name api_token > provision/encrypted_tokens.yml
+TOKENS=(
+	api_token
+	runner_token
+)
 
 # Generate a password for a regular GitLab user
 </dev/urandom tr -dc _A-Za-z0-9 | head -c32 > secrets/user_password
-ansible-vault encrypt_string $(<secrets/user_password) --name user_password >> provision/encrypted_tokens.yml
+ansible-vault encrypt_string $(<secrets/user_password) --name user_password > provision/encrypted_tokens.yml
+
+for token in ${TOKENS[@]}; do
+  if [[ ! -f secrets/$token ]]; then
+    echo "No $token found from secrets/ -directory! Aborting..."; exit
+  fi
+  ansible-vault encrypt_string $(<secrets/$token) --name $token >> provision/encrypted_tokens.yml
+done
+
+
+
+
