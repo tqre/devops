08276b4525ec1c007dedc3bc8bbf0608f8e730d0
tqre <tkuure@gmail.com>
2020-11-30 09:27:50 +0200

    Reformed token encryption helper script

--- encrypt_tokens.sh
+++ encrypt_tokens.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # Encrypt GitLab API-tokens and a regular user secrets to a variable file
+# The tokens have to be fetched manually from a working GitLab instance
+
+SEC_DIR=secrets
+ENC_FILE=provision/encrypted_tokens.yml
 
 TOKENS=(
-	api_token
-	runner_token
+	token_api
+	token_runner
 )
 
 for token in ${TOKENS[@]}; do
   if [[ ! -f secrets/$token ]]; then
-    echo "No $token found from secrets/ -directory! Aborting..."; exit
+    echo "No $token found. Aborting..."; exit
   fi
-  ansible-vault encrypt_string $(<secrets/$token) --name $token >> provision/encrypted_tokens.yml
+  ansible-vault encrypt_string $(<$SEC_DIRE/$token) --name $token >> $ENC_FILE
 done
 
 
