0120530e3557f5e97055cd5deb7e5b7c79b683ac
tqre <tkuure@gmail.com>
2020-12-01 10:56:48 +0200

    General configurations - week starts on Monday

--- gitlab-api/configure_gitlab.py
+++ gitlab-api/configure_gitlab.py
@@ -12,6 +12,12 @@ ses.verify = '../secrets/gitlab.crt'
 # Create a connection object
 gl = gitlab.Gitlab('https://gitlab.tqre.fi', private_token=token, session=ses)
 
-settings = gl.settings.get()
-settings.signup_enabled = False
-settings.save()
+# Configure the GitLab instance
+# https://docs.gitlab.com/ce/api/settings.html
+conf = gl.settings.get()
+conf.auto_devops_enabled = False
+conf.first_day_of_week = 1
+conf.signup_enabled = False
+
+
+conf.save()
