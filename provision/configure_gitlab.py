adacb9301d435f48f68ec047e5c537c7dea97ad2
tqre <tkuure@gmail.com>
2020-12-11 22:59:36 +0200

    Set default branch for selinux repo with python API

--- provision/configure_gitlab.py
+++ provision/configure_gitlab.py
@@ -2,9 +2,8 @@ import gitlab
 import requests
 
 # Authentication token has to be acquired manually after instance is up and running.
-
-with open('../secrets/token_api', 'r') as apitokenfile:
-    token_api = apitokenfile.read()
+with open('../secrets/token_api', 'r') as file:
+    token_api = file.read()
 
 # Self-signed certificate verification
 ses = requests.Session()
@@ -20,3 +19,8 @@ conf.auto_devops_enabled = False
 conf.first_day_of_week = 1
 conf.signup_enabled = False
 conf.save()
+
+# Set default branch to the GitLab -specific one with the CI file
+selinux_repo = gl.projects.get(gl.projects.list(search='selinux')[0].id)
+selinux_repo.default_branch = "gitlab-ci"
+selinux_repo.save()
