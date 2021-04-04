dbd4b1e1912be7a10257ad5a37bad953b5238b26
tqre <tkuure@gmail.com>
2021-04-04 14:34:35 +0300

    Use bundle 2.7

--- provision/config_files/gitlab-upgrade.sh
+++ provision/config_files/gitlab-upgrade.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(cd /usr/share/webapps/gitlab ; su -s /bin/sh gitlab -c "$(cat environment | xargs) bundle exec rake db:migrate")
+(cd /usr/share/webapps/gitlab ; su -s /bin/sh gitlab -c "$(cat environment | xargs) bundle-2.7 exec rake db:migrate")
 systemctl start gitlab.target
